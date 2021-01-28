db516ee29d06d28aa1960ba9fb793097ffe7ab72
Chris Yang <ychris@google.com>
2021-01-28 10:44:54 -0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,14 @@
 import 'dart:io' as io;
 
 void main(List<String> args) async {
+    print('Running command: "ls -l"');
+    final io.Process process0 = await io.Process.start('ls', ['-l']);
+    await io.stdout.addStream(process0.stdout);
+    await io.stderr.addStream(process0.stderr);
+    if (await process0.exitCode != 0) {
+      print("\nls -l first time failed");
+      throw 123;
+    }
     print('\nRunning command: "flutter version"');
     final io.Process process = await io.Process.start('flutter', ['--version']);
     await io.stdout.addStream(process.stdout);
@@ -17,7 +25,7 @@ void main(List<String> args) async {
     await io.stdout.addStream(process2.stdout);
     await io.stderr.addStream(process2.stderr);
     if (await process2.exitCode != 0) {
-      print("\nls -l failed");
+      print("\nls -l second time failed");
       throw 123;
     }
     print('success');
